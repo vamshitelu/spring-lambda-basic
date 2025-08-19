ff6e087005a4075cb1a8a289f6e57ba1874bddd1
vamsh <vamshikrishna.telu@gmail.com>
2025-08-19 18:13:24 +0530

    terraform scripts - modified -11

--- infra/main.tf
+++ infra/main.tf
@@ -121,6 +121,9 @@ resource "aws_api_gateway_deployment" "rest_api_deployment" {
   depends_on = [
     aws_api_gateway_integration.lambda_integration
   ]
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.rest_api.body))
+  }
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
 }
 #----------------------------------------
