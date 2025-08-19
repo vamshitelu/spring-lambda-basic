dd1e917bb4cd38fc90c79ac83883298fb9490433
vamsh <vamshikrishna.telu@gmail.com>
2025-08-19 19:00:22 +0530

    terraform scripts - modified -15

--- infra/main.tf
+++ infra/main.tf
@@ -84,25 +84,6 @@ data "aws_api_gateway_resource" "root" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
 }
 
-# Root Method (for "/")
-resource "aws_api_gateway_method" "root_method" {
-  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
-  resource_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-# Root Integration
-resource "aws_api_gateway_integration" "root_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
-  resource_id             = aws_api_gateway_rest_api.rest_api.root_resource_id
-  http_method             = aws_api_gateway_method.root_method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${aws_lambda_function.springboot_lambda_basic.arn}/invocations"
-}
-
-
 #----------------------------------------
 #Resource proxy
 #----------------------------------------
@@ -139,11 +120,13 @@ resource "aws_api_gateway_integration" "lambda_integration" {
 resource "aws_api_gateway_deployment" "rest_api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
   triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.rest_api.body))
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_rest_api.rest_api.body,
+      aws_api_gateway_integration.lambda_integration.uri,
+    ]))
   }
   depends_on = [
-    aws_api_gateway_integration.lambda_integration,
-    aws_api_gateway_integration.root_integration
+    aws_api_gateway_integration.lambda_integration
   ]
 
 }
