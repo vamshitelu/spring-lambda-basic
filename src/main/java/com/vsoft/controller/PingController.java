41d8dcefac81dd4c699819167a29d20696a58fa2
vamsh <vamshikrishna.telu@gmail.com>
2025-08-19 19:55:40 +0530

    terraform scripts - modified -17

--- src/main/java/com/vsoft/controller/PingController.java
+++ src/main/java/com/vsoft/controller/PingController.java
@@ -1,20 +1,24 @@
 package com.vsoft.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.HashMap;
 import java.util.Map;
 
-
+@Slf4j
 @RestController
 @EnableWebMvc
 public class PingController {
+
     @RequestMapping(path = "/ping", method = RequestMethod.GET)
     public Map<String, String> ping() {
+        log.info("Ping method called.");
         Map<String, String> pong = new HashMap<>();
         pong.put("pong", "Hello, World!");
+        log.info("Exit from ping method");
         return pong;
     }
 }
--- src/main/java/com/vsoft/service/CourseService.java
+++ src/main/java/com/vsoft/service/CourseService.java
@@ -1,39 +1,48 @@
 package com.vsoft.service;
 
 import com.vsoft.model.Course;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 public class CourseService {
 
     List<Course> list = new ArrayList<>();
 
     public List<Course> getAllCourses(){
+        log.info("getAllCourses method called.");
+        log.info("Course List size :"+list.size());
         return list;
     }
 
     public Course getCourseById(Long id){
+        log.info("getCourseById method called.");
         return list.stream().filter(x->x.getId().equals(id)).findFirst().get();
     }
 
     public Course addCourse(Course course){
+        log.info("addCourse method called.");
         list.add(course);
         return course;
     }
 
     public void deleteCourse(Long id){
+        log.info("deleteCourse method called.");
         list.removeIf(x->x.getId().equals(id));
     }
 
     public Course updateCourse(Course course){
+        log.info("updateCourse method called.");
         Course existingCourse = list.stream().filter(x->x.getId().equals(course.getId())).findFirst().get();
         existingCourse.setCourseName(course.getCourseName());
         existingCourse.setDuration(course.getDuration());
         existingCourse.setAmount(course.getAmount());
         list.add(existingCourse);
+        log.info("Course updated successfully");
         return existingCourse;
     }
 }
